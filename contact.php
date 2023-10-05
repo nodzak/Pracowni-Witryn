ba4716e86756e525da8dd6d2dfdb6e88cd0116bf
nodzak <145668680+nodzak@users.noreply.github.com>
2023-10-05 07:32:35 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+<?php
+        //require('./elements/navbar.php');
+        require_once('./elements/navbar.php');
+    ?>
+    <main>
+        <h1>Contact</h1>
+    </main>
+    <?php
+        //require('./elements/footer.php');
+        require_once('./elements/footer.php');
+    ?>
+</body>
+</html>
