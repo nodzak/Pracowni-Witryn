438b4038e3810458321d990b7533552088b72bea
nodzak <145668680+nodzak@users.noreply.github.com>
2023-10-05 13:38:06 +0200

    Add files via upload

---
+++ 05.10/common/header.php
@@ -0,0 +1,13 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Strona Główna</a></li>
+        <li><a href="about-us.php">O nas</a></li>
+        <li><a href="contact.php">Kontakt</a></li>
+
+
+    </ul>
+
+
+
+
+</nav>
