7a211cc5d9e1cebafa382bf88e5166723084f617
nodzak <145668680+nodzak@users.noreply.github.com>
2023-11-02 13:58:19 +0100

    Add files via upload

---
+++ 02.11/include/db.php
@@ -0,0 +1,60 @@
+<?php
+/*
+$conn = mysqli_connect(
+    hostname:'127.0.0.1',
+    username:'php',
+    password:'php',
+    database:'4BG2',
+    port:'3306'
+);
+
+if(mysqli_connect_errno()) {
+    echo "failed to conntect", mysqli_connect_error();
+}
+else {
+echo "Connection successful";
+}
+*/
+
+$conn = new mysqli(
+    hostname:'127.0.0.1',
+    username:'php',
+    password:'php',
+    database:'4BG2',
+    port:'3306'
+);
+if ($conn->connect_errno) {
+    echo "failed to conntect", mysqli_connect_error(); 
+    die;
+}
+
+function storeMessage(string $email, string $message): bool 
+{
+    global $conn;
+    $sql = sprintf(
+        'INSERT INTO contact_messeges (email, message values ("%s","%s")',
+        $email,
+        $message
+    );
+    $result = $conn->query($sql);
+    $conn->close();
+    return $result;
+}
+
+function findAdminByLogin(string $login): ?array{
+    global $conn;
+
+    $sql = sprintf(
+        'SELECT * FROM admin WHERE login',
+        $login
+    );
+
+    $result = $conn->query($sql);
+
+    if ($row = $result->fetch_assoc()) {
+        $result->free_result();
+
+        return $row;
+    }
+    return null;
+}
